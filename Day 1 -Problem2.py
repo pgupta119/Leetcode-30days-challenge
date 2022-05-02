1a5f2288ffa9555402b3a05bf3c221436cf14492
pgupta119 <guptaprakash1995119@gmail.com>
2022-05-02 09:06:26 +0200

    Day 1 Problem 2 to find the string is valid

--- Day 1 -Problem2.py
+++ Day 1 -Problem2.py
@@ -4,3 +4,33 @@
 #
 # 1.Open brackets must be closed by the same type of brackets.
 # 2. Open brackets must be closed in the correct order.
+
+# my solution ( partially work)
+
+class Solution:
+    def isValid(self, s: str) :
+        if (("[" in s) & ("]" in s)) | (("(" in s) & (")" in s)) | (("{" in s) & ("}" in s)):
+            return True #checking for the any of the pairs are present in the string.
+        # this condition will fail when bairs of the brackets are more
+        else:
+            return False
+# Not able to find out  how to check the correct order of the brackets
+
+# actual solution  using stack
+
+class Solution:
+    def isValid(self, s: str) :
+        stack = [0]
+        mapping = {0: None, '(': ')', '[': ']', '{': '}'}
+        for c in s:
+            if c in mapping:
+                stack.append(c)
+            else:
+                if mapping[stack.pop()] != c:
+                    return False
+        return stack == [0]
+
+st = Solution()
+print(st.isValid("{({[]})}"))
+
+
