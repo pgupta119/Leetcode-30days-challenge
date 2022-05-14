1a96334742bf7a8c5ab03d272bd3416673ccb433
pgupta119 <guptaprakash1995119@gmail.com>
2022-05-14 19:45:02 +0200

    Contains Duplicates number

---
+++ Day-5.py
@@ -0,0 +1,28 @@
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+# My solution
+class Solution:
+    def containsDuplicate(nums: List[int]):
+        count = 0
+        for i in range(0, len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] == nums [j]:
+                    count = count +1
+        if count > 0 :
+            return True
+        else:
+            return False
+
+# Problem with this solution is to Time complexity O(N*N)
+
+
+# Better approach is to find length of the distinct number length and compare with actual length of the list
+
+
+
+class Solution:
+    def containsDuplicate(nums: List[int]):
+        return True if len(set(nums)) < len(nums) else False
+
+
+
