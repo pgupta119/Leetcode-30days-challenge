b73537b66d232e6fd1e4ef5b64f1cddaf62bd81e
pgupta119 <guptaprakash1995119@gmail.com>
2022-05-21 23:57:54 +0200

    Best Time to Buy and Sell Stocks with optimal solution

--- Day-9.py
+++ Day-9.py
@@ -37,6 +37,18 @@ class Solution:
 
 # optimal solution :
 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            elif prices[i] - min_price > max_profit:
+                max_profit = prices[i] - min_price
+
+        return max_profit
+
 
 
 
