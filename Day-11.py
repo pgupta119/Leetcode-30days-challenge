df188665f5fbaa78a58a0fd0fb5b98984bb6f4a4
pgupta119 <guptaprakash1995119@gmail.com>
2022-05-25 23:04:31 +0200

    first unique character in a string.

---
+++ Day-11.py
@@ -0,0 +1,12 @@
+# Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+#Solution :
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = collections.Counter(s)
+        for idx,ch in enumerate(s):
+            if count[ch] == 1:
+                return idx
+        return -1
+
+    
