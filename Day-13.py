b854de1ef5341fb353d0f844827cb354e204c42b
pgupta119 <guptaprakash1995119@gmail.com>
2022-05-28 23:29:03 +0200

    Climbing Stairs solution.

--- Day-13.py
+++ Day-13.py
@@ -1,3 +1,15 @@
 # You are climbing a staircase. It takes n steps to reach the top.
 #
 # Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+#Solution
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        last, s_last = 1, 1
+        for i in range(n - 1):
+            temp = last
+            last = last + s_last
+            s_last = temp
+
+        return last
