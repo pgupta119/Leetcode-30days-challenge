8e3de58907fd2f55bb6c2a07e2b317f93c2b548b
pgupta119 <guptaprakash1995119@gmail.com>
2022-05-31 19:52:05 +0200

    Guess Number Higher or Lower with other solution.

--- Day-15.py
+++ Day-15.py
@@ -24,3 +24,17 @@ class Solution:
         # for i in range(0,n+1):
         if (guess(i) == 0):
             return i
+
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        s, p = 0, n
+        while s <= p:
+            m = s + (p - s) // 2
+            if (guess(m) == 0):
+                return m
+            if (guess(m) == 1):
+                s = m + 1
+            else:
+                p = m - 1
+
