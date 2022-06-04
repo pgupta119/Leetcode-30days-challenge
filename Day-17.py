6cf57fd9c1fcd050920ca8355f20dc6da4f181e1
pgupta119 <guptaprakash1995119@gmail.com>
2022-06-04 23:53:57 +0200

    Valid prefect Square solutionw

--- Day-17.py
+++ Day-17.py
@@ -1,3 +1,15 @@
 # Given a positive integer num, write a function which returns True if num is a perfect square else False.
 #
 # Follow up: Do not use any built-in library function such as sqrt.
+
+#My solution
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num < 2:
+            return True
+
+        x = num // 2
+        while x * x > num:
+            x = (x + num // x) // 2
+        return x * x == num
